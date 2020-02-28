77e5fde6cba139c390558a73002f7f97c6e7aca1
Michael Otty <michael.otty@gmail.com>
2020-02-28 18:56:00 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,33 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class SRS:
+    def __init__(self, waveform, fs, q=10, f=12):
+        """Shock Response Spectrum"""
+
+        self.input = waveform
+        self.fs = fs
+        self.spectrum = [i for i in range(100)]
+
+        f = 500.0
+        dt = 1/fs
+        phi = 0.05
+        omega_n = f * 2 * np.pi
+        omega_d = omega_n*np.sqrt(1-phi**2)
+
+        a = 2 * np.exp(-phi * omega_n * dt) * np.cos(omega_d * dt)
+        b = -np.exp(-2 * phi * omega_n * dt)
+        c = 2 * phi * omega_n * dt
+        d = omega_n * dt * np.exp(-phi * omega_n * dt)*((omega_n/omega_d * (1-2*phi**2))* np.sin(omega_d*dt)-2*phi*np.cos(omega_d*dt))
+        e = 0
+
+        num = [c, d, e]
+        den = [1, -a, -b]
+
+        w, h = signal.freqz(num, den)
+
+        plt.plot(w/(2*np.pi)*fs, np.abs(h))
+        plt.show()
+
